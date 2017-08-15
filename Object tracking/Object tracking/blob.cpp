4545bba4a42fd58948ecb11a1717f6700a096184
Siddharth M <DrCurry@users.noreply.github.com>
2017-08-15 01:13:53 -0400

    Add files via upload
    Removed a whole lot of variables that did nothing.

--- Object tracking/Object tracking/blob.cpp
+++ Object tracking/Object tracking/blob.cpp
@@ -1,19 +1,11 @@
-// Blob.cpp
-
 #include "Blob.h"
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
-Blob::Blob(std::vector<cv::Point> _contour) {
-
+Blob::Blob(std::vector<cv::Point> _contour) 
+{
 	contour = _contour;
-
 	boundingRect = cv::boundingRect(contour);
-
-	centerPosition.x = (boundingRect.x + boundingRect.x + boundingRect.width) / 2;
-	centerPosition.y = (boundingRect.y + boundingRect.y + boundingRect.height) / 2;
-
+	centerPosition.x = (2 * boundingRect.x + boundingRect.width) / 2;
+	centerPosition.y = (2 * boundingRect.y + boundingRect.height) / 2;
 	dblDiagonalSize = sqrt(pow(boundingRect.width, 2) + pow(boundingRect.height, 2));
-
 	dblAspectRatio = (float)boundingRect.width / (float)boundingRect.height;
-
 }
--- Object tracking/Object tracking/main.cpp
+++ Object tracking/Object tracking/main.cpp
@@ -1,17 +1,12 @@
-// ImageSubtractionCpp.sln
-// main.cpp
-
 #include<opencv2/core/core.hpp>
 #include<opencv2/highgui/highgui.hpp>
 #include<opencv2/imgproc/imgproc.hpp>
-
+#include <stdlib.h>
 #include<iostream>
-//#include<conio.h>           // it may be necessary to change or remove this line if not using Windows
-
 #include "Blob.h"
-
 #include <chrono>
 
+#pragma region Trackbar function prototypes
 void low_B_thresh_trackbar(int, void*);
 void low_G_thresh_trackbar(int, void*);
 void low_R_thresh_trackbar(int, void*);
@@ -20,42 +15,37 @@ void high_G_thresh_trackbar(int, void*);
 void high_R_thresh_trackbar(int, void*);
 void rectangleArea_thresh_trackbar(int, void*);
 void rectangle_Width_thresh_trackbar(int, void*);
+#pragma endregion
+#pragma region Global Variables
+const cv::Scalar COLOR_1 = cv::Scalar(0.0, 0.0, 0.0);
+const cv::Scalar COLOR_2 = cv::Scalar(255.0, 255.0, 255.0);
+const cv::Scalar COLOR_3 = cv::Scalar(255.0, 0.0, 0.0);
+const cv::Scalar COLOR_4 = cv::Scalar(0.0, 200.0, 0.0);
+const cv::Scalar COLOR_5 = cv::Scalar(0.0, 0.0, 255.0);
+const cv::Scalar COLOR_6 = cv::Scalar(255.0, 127.0, 150);
 
-// global variables ///////////////////////////////////////////////////////////////////////////////
-const cv::Scalar SCALAR_BLACK = cv::Scalar(0.0, 0.0, 0.0);
-const cv::Scalar SCALAR_WHITE = cv::Scalar(255.0, 255.0, 255.0);
-const cv::Scalar SCALAR_BLUE = cv::Scalar(255.0, 0.0, 0.0);
-const cv::Scalar SCALAR_GREEN = cv::Scalar(0.0, 200.0, 0.0);
-const cv::Scalar SCALAR_RED = cv::Scalar(0.0, 0.0, 255.0);
-const cv::Scalar SCALAR_PURPLE = cv::Scalar(255.0, 127.0, 150);
-
-int rectArea = 0, maxArea = 1000;
+int rectArea = 0, maxArea = 2000;
 int rectWidth = 1, maxWidth = 20;
 int low_B = 0, low_G = 0, low_R = 160;
 int high_B = 79, high_G = 65, high_R = 255;
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
-int main(void) {
+cv::Mat imgFrame1;
+cv::Mat RedBlob;
 
+cv::Mat structuringElement3x3 = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(3, 3));
+cv::Mat structuringElement5x5 = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(5, 5));
+cv::Mat structuringElement7x7 = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(7, 7));
+cv::Mat structuringElement9x9 = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(9, 9));
+#pragma endregion
+
+int main(void) 
+{
 	cv::namedWindow("trackbars", cv::WINDOW_AUTOSIZE);
 	cv::namedWindow("blob params", cv::WINDOW_AUTOSIZE);
 	cv::VideoCapture webcam(0);
-	webcam.set(CV_CAP_PROP_FRAME_WIDTH, 480);
-	webcam.set(CV_CAP_PROP_FRAME_HEIGHT, 320);
+	webcam.set(CV_CAP_PROP_FRAME_WIDTH, 640);
+	webcam.set(CV_CAP_PROP_FRAME_HEIGHT, 560);
 
-
-	cv::Mat imgFrame1;
-	cv::Mat imgFrame2;
-	cv::Mat imgDifference;
-	cv::Mat imgThresh;
-	cv::Mat RedBlob1, RedBlob2;
-
-	cv::Mat structuringElement3x3 = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(3, 3));
-	cv::Mat structuringElement5x5 = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(5, 5));
-	cv::Mat structuringElement7x7 = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(7, 7));
-	cv::Mat structuringElement9x9 = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(9, 9));
-
-	//Trackbars ///////////////////////////////////////////////////////////////////////////////////
 	cv::createTrackbar("Rect area", "blob params", &rectArea, maxArea, rectangleArea_thresh_trackbar);
 	cv::createTrackbar("Rect width", "blob params", &rectWidth, maxWidth, rectangle_Width_thresh_trackbar);
 	cv::createTrackbar("Low Blue", "trackbars", &low_B, 255, low_B_thresh_trackbar);
@@ -65,66 +55,35 @@ int main(void) {
 	cv::createTrackbar("Low Red", "trackbars", &low_R, 255, low_R_thresh_trackbar);
 	cv::createTrackbar("High Red", "trackbars", &high_R, 255, high_R_thresh_trackbar);
 
-
-
-	while ((char)cv::waitKey(16) != 'q') {
+	while ((char)cv::waitKey(36) != 'q') {
 		webcam >> imgFrame1;
-		webcam >> imgFrame2;
-		if (imgFrame1.empty() || imgFrame2.empty())
+		if (imgFrame1.empty())
 		{
 			break;
 		}
-		//auto start = std::chrono::high_resolution_clock::now();
-
 
 		std::vector<Blob> blobs;
-		cv::inRange(imgFrame1, cv::Scalar(low_B, low_G, low_R), cv::Scalar(high_B, high_G, high_R), RedBlob1);
-		cv::inRange(imgFrame2, cv::Scalar(low_B, low_G, low_R), cv::Scalar(high_B, high_G, high_R), RedBlob2);
-		cv::imshow("red1", RedBlob1);
-		cv::imshow("red2", RedBlob2);
-		cv::Mat imgFrame1Copy = RedBlob1.clone();
-		cv::Mat imgFrame2Copy = RedBlob2.clone();
+		cv::inRange(imgFrame1, cv::Scalar(low_B, low_G, low_R), cv::Scalar(high_B, high_G, high_R), RedBlob);
+		cv::imshow("red", RedBlob);
 
-		//cv::cvtColor(imgFrame1Copy, imgFrame1Copy, CV_BGR2GRAY);
-		//cv::cvtColor(imgFrame2Copy, imgFrame2Copy, CV_BGR2GRAY);
-		cv::imshow("img1", imgFrame1Copy);
-		cv::imshow("img2", imgFrame2Copy);
-
-		cv::GaussianBlur(imgFrame1Copy, imgFrame1Copy, cv::Size(3, 3), 0);
-		cv::GaussianBlur(imgFrame2Copy, imgFrame2Copy, cv::Size(3, 3), 0);
-
-		cv::absdiff(imgFrame1Copy, imgFrame2Copy, imgDifference);
-		cv::imshow("imgdif", imgDifference);
-
-		cv::threshold(imgDifference, imgThresh, 30, 255.0, CV_THRESH_BINARY);
-
-		cv::imshow("imgThresh", imgThresh);
-
-
-
-		cv::dilate(imgThresh, imgThresh, structuringElement5x5);
-		cv::dilate(imgThresh, imgThresh, structuringElement5x5);
-		cv::erode(imgThresh, imgThresh, structuringElement5x5);
-
-		cv::Mat imgThreshCopy = imgThresh.clone();
+		cv::dilate(RedBlob, RedBlob, structuringElement5x5);
+		cv::dilate(RedBlob, RedBlob, structuringElement3x3);
 
 		std::vector<std::vector<cv::Point> > contours;
-
-		cv::findContours(imgThreshCopy, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
-
-		cv::Mat imgContours(imgThresh.size(), CV_8UC3, SCALAR_BLACK);
-
-		cv::drawContours(imgContours, contours, -1, SCALAR_WHITE, -1);
-
+		cv::findContours(RedBlob, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+		cv::Mat imgContours(RedBlob.size(), CV_8UC3, COLOR_1);
+		cv::drawContours(imgContours, contours, -1, COLOR_2, -1);
 		cv::imshow("imgContours", imgContours);
 
 		std::vector<std::vector<cv::Point> > convexHulls(contours.size());
 
-		for (unsigned int i = 0; i < contours.size(); i++) {
+		for (unsigned int i = 0; i < contours.size(); i++)
+		{
 			cv::convexHull(contours[i], convexHulls[i]);
 		}
 
-		for (auto &convexHull : convexHulls) {
+		for (auto &convexHull : convexHulls)
+		{
 			Blob possibleBlob(convexHull);
 
 			if (possibleBlob.boundingRect.area() > rectArea &&
@@ -137,37 +96,28 @@ int main(void) {
 			}
 		}
 
-		cv::Mat imgConvexHulls(imgThresh.size(), CV_8UC3, SCALAR_BLACK);
+		cv::Mat imgConvexHulls(RedBlob.size(), CV_8UC3, COLOR_1);
 
 		convexHulls.clear();
 
-		for (auto &blob : blobs) {
+		for (auto &blob : blobs)
+		{
 			convexHulls.push_back(blob.contour);
+			cv::drawContours(imgConvexHulls, convexHulls, -1, COLOR_2, -1);  // for each blob
+			cv::rectangle(imgFrame1, blob.boundingRect, COLOR_6, 2);		 // draw a box around the blob
+			cv::circle(imgFrame1, blob.centerPosition, 3, COLOR_4, -1);		 // draw a filled-in circle at the center
 		}
-
-		cv::drawContours(imgConvexHulls, convexHulls, -1, SCALAR_WHITE, -1);
 
 		cv::imshow("imgConvexHulls", imgConvexHulls);
+		cv::imshow("imgFrame1", imgFrame1);
 
-		imgFrame2Copy = imgFrame2.clone();          // get another copy of frame 2 since we changed the previous frame 2 copy in the processing above
-
-		for (auto &blob : blobs) {                                                  // for each blob
-			cv::rectangle(imgFrame2Copy, blob.boundingRect, SCALAR_PURPLE, 2);             // draw a box around the blob
-			cv::circle(imgFrame2Copy, blob.centerPosition, 3, SCALAR_GREEN, -1);        // draw a filled-in circle at the center
-		}
-
-		cv::imshow("imgFrame2Copy", imgFrame2Copy);
-
-		// now we prepare for the next iteration
-
-		imgFrame1 = imgFrame2.clone();           // move frame 1 up to where frame 2 is
-		//auto finish = std::chrono::high_resolution_clock::now();
-		//std::chrono::duration<double> elapsed = finish - start;
-		//std::cout << elapsed.count() << std::endl;
 	}
+
+
 	return(0);
 
 }
+#pragma region Trackbar callbacks
 void rectangleArea_thresh_trackbar(int, void*)
 {
 
@@ -200,3 +150,4 @@ void high_G_thresh_trackbar(int, void *)
 void high_R_thresh_trackbar(int, void *)
 {
 }
+#pragma endregion
