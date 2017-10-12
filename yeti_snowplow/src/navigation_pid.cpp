9dcef325321acbc4dbe095a15f47726548c1020a
EpicWolverine <epicwolverine@me.com>
2017-10-12 15:21:38 -0400

    Added auto_control_logic and did some cleanup

--- yeti_snowplow/src/navigation_pid.cpp
+++ yeti_snowplow/src/navigation_pid.cpp
@@ -12,7 +12,22 @@
 #include <vector>
 using namespace std;
 
-#include "containers/CVAR.h"
+struct CVAR{
+	double targdist;
+    double targbearing;
+    double front;
+    double right;
+    double speed;               // between -1 to 1
+    double turn;                // between -1 to 1
+    double pErr;
+    double lastpErr;
+    double kP;
+    double dErr;
+    double kD;
+    double iErr;
+    double kI;
+    double lookAhead;
+};
 
 ros::Publisher turnPub;
 ros::ServiceClient waypointClient;
